f448a5d484573c3e4cb50ef13fd4696a19215920
BucharestNinja <mkyh765@gmail.com>
2019-09-10 23:58:45 +0900

    ゲーム部分制作進行

--- frontend/src/GamePage.js
+++ frontend/src/GamePage.js
@@ -8,12 +8,22 @@ export default class GamePage extends React.Component {
     console.log(this.props.playersDeck[0])
     this.state = {
       nowPlayer : 0,
-      playersDeck : this.props.playersDeck
+      playersDeck : this.props.playersDeck,
+      hand: []
     }
-    let cardNo =[];
+    this.cardNo =[];
+    this.drawCount = 0;
     for(let i =0; i<this.props.playerNum; i++){
-      cardNo[i]=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-      console.log(cardNo)
+      this.cardNo[i]=['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15'];
+      var n = this.cardNo[i].length;
+      var temp, j;
+      while (n) {
+          j = Math.floor(Math.random() * n--);
+          temp = this.cardNo[i][n];
+          this.cardNo[i][n] = this.cardNo[i][j];
+          this.cardNo[i][j] = temp;
+      }
+      console.log(this.cardNo[i])
     };
   }
 
@@ -30,17 +40,49 @@ export default class GamePage extends React.Component {
   }
 
   drawCard(){
-    let no =Math.floor(Math.random() * (1 - 15)) + 1
-    hand[] = state
+    //const hand_copy = this.state.hand.slice();
+  //  hand_copy[this.drawCount] = this.state.playersDeck[this.state.nowPlayer][0]['card'+this.cardNo[this.drawCount]]
+    this.setState({hand: this.state.hand.concat(this.state.playersDeck[this.state.nowPlayer][0]['card'+this.cardNo[this.drawCount][this.state.nowPlayer]])})
+    //this.state.hand.push(this.state.playersDeck[this.state.nowPlayer][0]['card'+this.cardNo[this.drawCount]])
+    console.log('card'+this.cardNo[this.drawCount][this.state.nowPlayer])
+    console.log(this.state.hand)
+    console.log(this.state.playersDeck[this.state.nowPlayer][0]['card'+this.cardNo[this.drawCount][this.state.nowPlayer]])
+    this.drawCount = this.drawCount+1
   }
+
+  renderCards(){
+    const rows = this.state.hand.map((card,index) =>
+  <tr key={card}>
+    <td>
+      {index + 1}
+    </td>
+    <td>{card}</td>
+  </tr>
+);
+
+  return (
+  <table border='1' cellSpacing='0'>
+     <thead>
+        <tr>
+           <th>Number</th>
+           <th>card</th>
+        </tr>
+     </thead>
+     <tbody>
+       {rows}
+     </tbody>
+  </table>
+)
+}
 
   render(){
     console.log(this.state.nowPlayer)
     return(
       <div>
         <p>{this.state.playersDeck[this.state.nowPlayer][0]['deckName']}</p>
-        <p></p>
-        <Button onClick={() =>this.PlayerChange()}>デッキ登録</Button>
+        <p>{this.renderCards()}</p>
+        <Button onClick={() =>this.PlayerChange()}>次のプレイヤー</Button>
+        <Button onClick={() =>this.drawCard()}>ドロー</Button>
       </div>
       )
   }

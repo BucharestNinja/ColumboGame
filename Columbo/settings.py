fc7f5807ab4008c4ff3fffb9751ecaecb734548d
BucharestNinja <46449327+BucharestNinja@users.noreply.github.com>
2019-11-18 23:30:22 +0900

    Update settings.py

--- Columbo/settings.py
+++ Columbo/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
-    'django_filters',
+    'django_filter',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
